be2a528841bda5c54acedf716b3485032b333c0c
PlanetEffiacy <jspevack@gmail.com>
2016-06-23 09:01:46 -0600

    Add real_time encryptor

--- encryptor.rb
+++ encryptor.rb
@@ -56,6 +56,50 @@ class Encryptor
       end
     end
 
-    def
+    def get_rotation_hash(message)
+      rotation_message_hash = { "rotation" => message.split("<<")[1].split(">>")[0].to_i,
+                                "message" => message.split("<<")[0] }
+    end
+
+    def encrypt_realtime(message, selection)
+
+      rotation = 9
+
+      if (message.include? "<<") && (message.include? ">>")
+        rotation_message_hash = get_rotation_hash(message)
+        message = rotation_message_hash["message"]
+        rotation = rotation_message_hash["rotation"]
+      end
+
+      if selection == 1
+        puts encrypt(message, rotation)
+      else
+        puts decrypt(message, rotation)
+      end
+
+    end
+
+    def real_time
+      puts "Welcome to Real Time Encryptor!"
+      selection = ""
+      until ((selection == 1) || (selection == 2)) do
+        selection = input_select
+      end
+
+      if (selection == 1)
+        puts "Type your message to encrypt: "
+      elsif (selection == 2)
+        puts "Type your encrypted message to decrypt: "
+      end
+
+      message = gets.chomp
+      encrypt_realtime(message, selection)
+
+    end
+
+    def input_select
+      puts "Select [1] Encrypt or [2] Decrypt"
+      selection = gets.chomp.to_i
+    end
 
 end
